e794f1acefee15739bad15440de2828149e35735
Synthmax <max@synthmax.dk>
2013-12-29 19:41:00 +0100

    -

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# Add additional locales
+
+bash "Add a hello-world text file" do
+  code <<-EOC
+     touch hello-world.txt
+     echo hello $user > hello-world.txt
+  EOC
+end
+
