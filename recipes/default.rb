0adeb531d08479c3bfb73067bac176efcfc2f18c
Synthmax <max@synthmax.dk>
2013-12-29 20:01:36 +0100

    2

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,7 +13,7 @@ bash "Add a hello-world text file" do
   user "root"
   code <<-EOC
      touch hello-world.txt
-     echo hello $USER > hello-world.txt
+     echo hello $USER > hello-world2.txt
   EOC
 end
 
